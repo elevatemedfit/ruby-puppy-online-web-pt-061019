1c18ed5f4be26932ea2684aa26cac151e8d2c881
Chris Junker <elevatemedfit@gmail.com>
2019-07-08 12:45:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Add your code here
+class Dog
+
+  @@all = []
+
+  attr_accessr0 :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end# Add your code here
